30e93f476a1b94a8d1eac37aca0d3b0340683463
Dario <dario.strunjak@alfatec.hr>
2022-06-06 01:51:43 +0200

    Error fix

--- code/dodatak_A.py
+++ code/dodatak_A.py
@@ -1,4 +1,6 @@
 import getpass
+import hashlib
+from math import nan
 
 
 class OperationsManager():
@@ -9,13 +11,15 @@ class OperationsManager():
 
     def perform_division(self) -> float:
         """Divides a with b. If b is zero, returns NaN."""
+        if b==0: return nan
         return self.a / self.b
 
 
 if __name__ == "__main__":
     user = input("Username: ")
     password = getpass.getpass("Password: ")
-    if user != "root" or password != "123":
+    pwd_hash = hashlib.sha256(password.encode()).hexdigest()
+    if user != "root" or pwd_hash != "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3":
         print("Wrong username or password!")
         exit(0)
     else:
